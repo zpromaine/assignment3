1c9627dbb63190abff5651d7ddd52664e755d3e3
Zach Romaine <zpromaine@gmail.com>
2026-02-08 20:11:11 -0500

    added test code for subtraction

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,6 +4,10 @@ from app.operations import Operations
 
 Number = Union[int, float]
 
+# ---------------------------------------------
+# Unit Tests for the 'addition' Method
+# ---------------------------------------------
+
 @pytest.mark.parametrize(
     "a, b, expected",
     [
@@ -25,4 +29,32 @@ def test_addition(a: Number, b:Number, excpected: Number) -> None:
     result = Operations.addition(a, b)
 
     assert result == expected, f"Expected addition({a}, {b}) to be {expected}, but got {results}"
-    
+
+
+@pytest.mark.parametrize
+
+# ---------------------------------------------
+# Unit Tests for the 'subtraction' Method
+# ---------------------------------------------
+
+@pytest.mark.parametrize(
+    "a, b, excpected",
+    [
+        (5, 3, 2),
+        (0, 0, 0),
+        (-5, -3, -2),
+        (10.5, 5.5, 5.0),
+        (-10.5, -5.5, -5.0),
+    ]
+    ids=[
+        "subtract_two_positive_inters",
+        "subtract_two_zeros",
+        "subtract_negative_from negative_integer",
+        "subtract_two_positive_floats",
+        "subtract_two_negative_floats",
+    ]
+)
+def test_subtraction(a: Number, b: Number, expected: Number) -> None:
+      result = Operations.subtraction(a, b)
+
+    assert result == expected, f"Expected subtraction({a}, {b}) to be {expected}, but got {results}"
