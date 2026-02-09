fe5d4c9cadb64b139955aab7b14edf8137a972fb
Zach Romaine <zpromaine@gmail.com>
2026-02-08 19:27:19 -0500

    Rewrote App-Operations-Init

---
+++ app/operations/__init__.py
@@ -0,0 +1,23 @@
+
+
+
+class Operations:
+
+    @staticmethod
+    def addition(a: float, b:float) -> float:
+        return a + b
+    
+    @staticmethod
+    def subtraction(a: float, b:float) -> float:
+        return a - b
+    
+    @staticmethod
+    def multiplication(a: float, b:float) -> float:
+        return a * b
+    
+    @staticmethod
+    def division(a: float, b:float) -> float:
+        if b == 0
+            raise ValueError("Division by zero is not allowed.")
+        return a / b
+        
